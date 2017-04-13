c9ef9ec79957bb61e66cd2bc3f701406b53acc93
charles <charles.ferreira@aluno.ufabc.edu.br>
2017-04-13 14:53:20 -0300

    Mudança de Disponibilidade para Disp e método para buscar e ordenar por curso

--- src/java/facade/OfertaDisciplinaFacade.java
+++ src/java/facade/OfertaDisciplinaFacade.java
@@ -10,8 +10,8 @@ import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
-
 
 @Stateless
 public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
@@ -22,12 +22,10 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
 
     @Override
     protected SessionFactory getSessionFactory() {
-
         return HibernateUtil.getSessionFactory();
-
     }
     
-    public OfertaDisciplina inicializarColecaoDisponibilidades(OfertaDisciplina oD){
+    /*public OfertaDisciplina inicializarColecaoDisponibilidades(OfertaDisciplina oD){
         
         Session session = getSessionFactory().openSession();
         session.refresh(oD);
@@ -35,10 +33,19 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
         Hibernate.initialize(oD.getDisponibilidades());
         session.close();
         return oD;
+    }*/
+    
+    //Retorna a lista de disponibilidades
+    public OfertaDisciplina inicializarColecaoDisponibilidades(OfertaDisciplina oD){
         
+        Session session = getSessionFactory().openSession();
+        session.refresh(oD);
+        Hibernate.initialize(oD);
+        Hibernate.initialize(oD.getDispo());
+        session.close();
+        return oD;   
     }
-    
-    
+
     /**
      * Filtra as Ofertas de disciplina por disciplina(s) com afinidades
      * e/ou turno e/ou campus e quadrimestre
@@ -51,7 +58,6 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
     public List<OfertaDisciplina> filtrarAfinidTurnCampQuad(List<Disciplina> disciplinas, String turno, String campus, int quadrimestre) {
 
         try {
-
             Session session = getSessionFactory().openSession();
 
             Criteria criteria = session.createCriteria(OfertaDisciplina.class);
@@ -59,30 +65,23 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
             if (!campus.equals("")) {
                 criteria.add(Restrictions.eq("campus", campus));
             }
-
             if (!turno.equals("")) {
                 criteria.add(Restrictions.eq("turno", turno));
             }
-
             if (quadrimestre != 0) {
                 criteria.add(Restrictions.eq("quadrimestre", quadrimestre));
             }
-
             if (!disciplinas.isEmpty()) { //Caso tenha sido escolhido para filtrar por disciplinas com afinidades
 
                 criteria.add(Restrictions.in("disciplina", disciplinas));
-
             }
-
             List resultado = criteria.list();
 
             session.close();
             return resultado;
-
         } catch (HibernateException e) {
             return null;
         }
-
     }
     
     /**
@@ -98,7 +97,6 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
     public List<OfertaDisciplina> filtrarEixoCursoTurnoCampusQuad(List<String> eixos, List<String> cursos, String turno, String campus, int quadrimestre) {
 
         try {
-
             Session session = getSessionFactory().openSession();
 
             Criteria criteria = session.createCriteria(OfertaDisciplina.class);
@@ -106,15 +104,12 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
             if (!campus.equals("")) {//Se o usuario escolheu filtrar pelo campus
                 criteria.add(Restrictions.eq("campus", campus));
             }
-
             if (!turno.equals("")) {//Se o usuario escolheu filtrar pelo turno
                 criteria.add(Restrictions.eq("turno", turno));
             }
-
             if (quadrimestre != 0) {//Se o usuario escolheu filtrar pelo quadrimestre
                 criteria.add(Restrictions.eq("quadrimestre", quadrimestre));
             }
-
             if (eixos.size() > 0) {//Caso algum filtro de eixo tenha sido selecionado
 
                 criteria.createAlias("disciplina", "d");
@@ -124,27 +119,21 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
                 } else {
                     criteria.add(Restrictions.in("d.eixo", eixos));
                 }
-
             } else {//Caso nenhum filtro de eixo tenha sido selecionado
 
                 if (cursos.size() > 0) {//Caso algum filtro de curso tenha sido selecionado
                     criteria.createAlias("disciplina", "d").add(Restrictions.or(Restrictions.in("d.curso", cursos)));
                 }
             }
-
             List resultado = criteria.list();
 
             session.close();
             return resultado;
-
         } catch (HibernateException e) {
             return null;
         }
     }
-    
-    
-    
-    
+
     /**
      * Lista as ofertas de disciplina por quadrimestre
      * @param quadrimestre int
@@ -163,6 +152,35 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
         return results;
     }
     
+    public List<OfertaDisciplina> ordenarCursos(int quadrimestre){
+        Session session = getSessionFactory().openSession();
+        Criteria crit = session.createCriteria(OfertaDisciplina.class);
+        crit.add(Restrictions.eq("quadrimestre", quadrimestre));
+        crit.addOrder(Order.asc("curso"));
+        crit.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+        
+        List results = crit.list();
+        
+        session.close();
+        return results;
+    }
+
+    public List<OfertaDisciplina> buscaCurso(List<String> cursos, int quadrimestre){
+        Session session = getSessionFactory().openSession();
+        Criteria crit = session.createCriteria(OfertaDisciplina.class);
+        crit.add(Restrictions.eq("quadrimestre", quadrimestre));
+        if (cursos.size() > 0) {
+            crit.createAlias("disciplina", "d").add(Restrictions.or(Restrictions.in("d.curso", cursos)));
+        }
+        crit.addOrder(Order.asc("curso"));
+        crit.addOrder(Order.asc("d.nome"));
+        crit.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+        
+        List results = crit.list();
+        
+        session.close();
+        return results;
+    }
     
     //Filtro por disciplina e/ou turno e/ou campus
 //    public List<OfertaDisciplina> filtrarDiscTurnoCampus(List<Disciplina> disciplinas, String turno, String campus) {
@@ -232,7 +250,6 @@ public class OfertaDisciplinaFacade extends AbstractFacade<OfertaDisciplina>{
 //        }
 //
 //    }
-
 }
 
 
